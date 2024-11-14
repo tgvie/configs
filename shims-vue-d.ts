4a7c8ffe3e7e467ea02499c9b2976d3decbcee42
Vie <166875380+tgvie@users.noreply.github.com>
2024-11-14 03:12:51 +0100

    Create shims-vue-d.ts

---
+++ shims-vue-d.ts
@@ -0,0 +1,19 @@
+// @see https://github.com/vuejs/vue-cli/issues/1198
+// src/shims-vue.d.ts
+declare module '*.vue';
+/*
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
+*/
+
+/*
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
+*/
+
+// if you use env variables c:
+// declare module '*/envs.ts';
