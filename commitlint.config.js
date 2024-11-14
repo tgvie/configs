e0386d8ebd93120afc0a4ca37b0f8f7a67aa2f3a
Vie <166875380+tgvie@users.noreply.github.com>
2024-11-14 03:10:54 +0100

    Create commitlint.config.js

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {extends: ['gitmoji']};
+
+/*
+Install:
+pnpm add -D @commitlint/cli @commitlint/config-conventional commitizen commitlint commitlint-config-gitmoji cz-conventional-changelog husky
+
+In package.json config section:
+
+"config": {
+  "commitizen": {
+    "path": "./node_modules/cz-conventional-changelog"
+  }
+}
+*/
